15f3f13698497882e580b282530d5f09275be607
Jon Clucas <jon.clucas@childmind.org>
2017-05-30 11:39:13 -0400

    document intentions

---
+++ SM_response_paradigm_analysis/SM_response_paradigm_analysis.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SM_response_paradigm_analysis.py
+
+Functions to compare vocal response conditions of the 2016 Child Mind Institute
+selective mutism EEG response paradigm.
+
+Authors:
+    – Jon Clucas, 2017 (jon.clucas@childmind.org)
+    – Arno Klein, 2017 (arno.klein@childmind.org)
+    
+© 2017, Child Mind Institute, Apache v2.0 License
+"""
+
+# TODO: EEG data
+# TODO: power analysis
+
+"""
+TODO: load voice data
+      2 within group / across conditions
+      within condition / across groups
+      identify most discriminative features
+      if necessary, feature reduction
+      threshold
+      load demograohic, diagnostic, & SMQ data
+      multinomial regression
+      LOOCV
+      `Elias Chaibub-Neto's predictive modeling functions
+      <https://github.com/ChildMindInstitute/PredictiveModelingPipeline>`_
+      Bayesian confidence score
+"""
+
