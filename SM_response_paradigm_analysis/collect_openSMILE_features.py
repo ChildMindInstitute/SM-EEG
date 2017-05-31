ff50a258878146a54138e5db49c7ce1b848bbcb2
Jon Clucas <jon.clucas@childmind.org>
2017-05-31 14:50:28 -0400

    PEP8

--- SM_response_paradigm_analysis/collect_openSMILE_features.py
+++ SM_response_paradigm_analysis/collect_openSMILE_features.py
@@ -8,27 +8,28 @@ analysis.
 
 Author:
     – Jon Clucas, 2017 (jon.clucas@childmind.org)
-    
+
 © 2017, Child Mind Institute, Apache v2.0 License
 """
 import argparse, os, pandas as pd, subprocess
 from configuration_defaults import openSMILE_directory
 
+
 def build_table(oS, audio, config):
     """
     Build and save
-    
+
     Parameters
     ----------
     oS : string
         path to openSMILE binary
-        
+  
     audio : string
         path to audio files
-        
+   
     config : string
         path to openSMILE config file
-        
+   
     Returns
     ------
     table : pandas dataframe
@@ -48,30 +49,31 @@ def build_table(oS, audio, config):
             os.remove(temp)
             features = dict(zip(features_all.iloc[:-2][features_all.columns[-1]
                        ], features_all.iloc[-1].values[1].split(",")))
-            table = table.append({"URSI":ursi, "stranger":condition, "trial":
-            str(int(trial[-3:])), **features}, ignore_index=True)
+            table = table.append({"URSI": ursi, "stranger": condition, "trial":
+                    str(int(trial[-3:])), **features}, ignore_index=True)
     return(table)
+
 
 def main():
     """
     Set up and run openSMILE
-    
+
     Parameters
     ----------
     (from command line)
     audio : string
         path to directory with audio files to process
-        
+
     config : string
         filename of openSMILE config file
-        
+
     outdir : string (optional)
         path to directory in which to save openSMILE outputs, default=
         [openSMILE_config] in parent directory of [audio]
-        
+
     openSMILE_directory : string (optional)
         path to openSMILE installation, default="../../../openSMILE-2.3.0"
-        
+
     Output
     ------
     features.csv : comma separated values file
@@ -87,10 +89,9 @@ def main():
                         "to save openSMILE outputs, default=[openSMILE_config]"
                         "in parent directory of [audio]"))
     parser.add_argument("-S", "--openSMILE", default=openSMILE_directory,
-                        metavar="openSMILE", dest=
-                        "openSMILE_directory", type=str, help=(
-                        "path to openSMILE installation, default=\"../../../op"
-                        "enSMILE-2.3.0\""))
+                        metavar="openSMILE", dest="openSMILE_directory", type=
+                        str, help=("path to openSMILE installation, default=\""
+                        "../../../openSMILE-2.3.0\""))
     arg = parser.parse_args()
     outdir = arg.outdir if arg.outdir else os.path.join(os.path.dirname(
              arg.audio.rstrip("/")), arg.config)
@@ -101,6 +102,7 @@ def main():
             "config", arg.config))
     print(table)
     table.to_csv(os.path.join(outdir, "features.csv"))
+
 
 # ============================================================================
 if __name__ == '__main__':
