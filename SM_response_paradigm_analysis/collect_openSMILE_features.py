689dfd13c34f00b6aa6e85a232f12bf892e5ae5b
Jon Clucas <jon.clucas@childmind.org>
2017-05-31 10:40:48 -0400

    begin response paradigm analysis

---
+++ SM_response_paradigm_analysis/collect_openSMILE_features.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+collect_openSMILE_features.py
+
+Functions to collect openSMILE features from a collection of sound files for
+analysis.
+
+Authors:
+    – Jon Clucas, 2017 (jon.clucas@childmind.org)
+    – Arno Klein, 2017 (arno.klein@childmind.org)
+    
+© 2017, Child Mind Institute, Apache v2.0 License
+"""
+
